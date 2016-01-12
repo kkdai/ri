9c0d940d9e07fe15ccdde551ca6142f5230edd85
Evan Lin <evanslin@gmail.com>
2016-01-12 16:44:48 +0800

    done internal port implement

--- example/client/main.go
+++ example/client/main.go
@@ -73,6 +73,17 @@ func GetIP() (net.IP, string) {
 	return nil, ""
 }
 
+func FindInternalPort(conn *net.UDPConn) string {
+	if len(conn.LocalAddr().String()) <= 0 {
+		return 0
+	}
+
+	log.Println("network:", conn.LocalAddr().Network(), " add:", conn.LocalAddr().String())
+	iport := conn.LocalAddr().String()[strings.Index(conn.LocalAddr().String(), ":")+1:]
+	log.Println("port:", iport)
+	return iport
+}
+
 func main() {
 	ServerAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:10001")
 	CheckError(err)
@@ -82,6 +93,9 @@ func main() {
 
 	Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
 	CheckError(err)
+	log.Println("network:", Conn.LocalAddr().Network(), " add:", Conn.LocalAddr().String())
+	iport := Conn.LocalAddr().String()[strings.Index(Conn.LocalAddr().String(), ":")+1:]
+	log.Println("port:", iport)
 
 	defer Conn.Close()
 
